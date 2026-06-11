2429a644a49077d55876016f6303f56ee5eef98f
yara22aulia <yara22aulia@gmail.com>
2026-06-11 19:02:07 +0700

    Menambahkan pembuatan node pada insert

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -74,9 +74,9 @@ public:
             }
         }
 
-        void inorder(Node* ptr) {
-            if (ROOT == NULL) 
-            {
+        void inorder(Node* ptr) 
+        {
+            if (ROOT == NULL) {
                 cout << "Tree is empty" << endl;
                 return;
             }
